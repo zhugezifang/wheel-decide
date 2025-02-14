00a6013d8f183a3571308e18c12ce6fa26e8402d
zhugezifang <xiaoqiu2017wy@163.com>
2025-02-14 09:48:20 +0000

    feat:wheel

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -32,29 +32,32 @@ export default async function Home({
   // 将 Set 转换为数组
   const randomThreeArray:any = Array.from(randomThree);
 
-  const post = await getPost('line-graph-maker', lang) as unknown as { 
+  /*const post = await getPost('line-graph-maker', lang) as unknown as { 
     title: string; 
     desc: string;
     contentHtml: string; 
-  }
+  }*/
 
   return (
     <>
     <main className="bg-gray-100 flex flex-col items-center w-full">
-     
+
+      {/*
       <div className="container mx-auto px-6 md:px-12 my-12 text-center">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">{post.title}</h1>
       </div>
+      */}
       
     </main>
 
-    <LineGraph />
-
-  
+    <iframe src="/html/wheel-decide.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1650px'}}></iframe>
+    
     <section className="bg-white py-2 my-10 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
+      {/*
       <article className="prose prose-gray dark:prose-invert mx-auto">
         <div className="mt-4" dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
       </article>
+      */}
     </section>
 
     <div className="bg-white py-2 my-6 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
