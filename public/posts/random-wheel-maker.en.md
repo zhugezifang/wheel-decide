52d15f76fc54dba1de1b5196532eb011d9d5ebd8
zhugezifang <xiaoqiu2017wy@163.com>
2025-02-17 14:05:13 +0000

    feat:random wheel

---
+++ public/posts/random-wheel-maker.en.md
@@ -0,0 +1,12 @@
+---
+title: "Randome Wheel Maker | Spin the Wheel"
+desc: "Random Wheel Maker is a powerful tool for making decisions using a fortune wheel. It's completely free and incredibly user-friendly. Simply enter your options, spin the wheel, and let it randomly select a name or choice for you. You can easily personalize the appearance of the wheel, and save or share it with others."
+---
+
+## What is Randome Wheel Maker?
+
+Random Wheel Maker is a powerful tool for making decisions using a fortune wheel. It's completely free and incredibly user-friendly. Simply enter your options, spin the wheel, and let it randomly select a name or choice for you. You can easily personalize the appearance of the wheel, and save or share it with others.
+
+## How do use Randome Wheel Maker?
+
+To use Randome Wheel Maker, simply enter your options in the text box, and click the "Spin" button. The wheel will spin, and a random name or choice will be selected for you. 
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -34,37 +34,36 @@ export default async function Home({
   // 将 Set 转换为数组
   const randomThreeArray:any = Array.from(randomThree);
 
-  /*const post = await getPost('line-graph-maker', lang) as unknown as { 
+  const post = await getPost('random-wheel-maker', lang) as unknown as { 
     title: string; 
     desc: string;
     contentHtml: string; 
-  }*/
+  }
 
   return (
     <>
     <main className="bg-gray-100 flex flex-col items-center w-full">
 
-      {/*
+      
       <div className="container mx-auto px-6 md:px-12 my-12 text-center">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">{post.title}</h1>
       </div>
-      */}
+      
       
     </main>
     <RandomWheel title="Your Random Wheel" optionParmas="Option 1,Option 2,Option 3,Option 4" />
 
-    
-
     <section className="bg-white py-2 my-10 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
       {/*
         <iframe src="/html/wheel-decide.html" allow="autoplay"  style={{ top: '0px', left: '0px',width: '100%', height: '1650px'}}></iframe>
-
+      */}
       <article className="prose prose-gray dark:prose-invert mx-auto">
         <div className="mt-4" dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
       </article>
-      */}
+      
     </section>
-
+    
+{/*
     <div className="bg-white py-2 my-6 px-4 mx-auto max-w-5xl sm:px-6 lg:px-8">
 
         <div className="container mx-auto px-6 md:px-12 my-6 text-center">
@@ -86,7 +85,7 @@ export default async function Home({
         </div>
 
     </div>
-
+*/}
     
     </>
   )
